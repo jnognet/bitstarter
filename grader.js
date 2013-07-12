109164f24f0de8db354fa03595836281c46128cb
Ubuntu <ubuntu@ip-10-251-67-45.us-west-2.compute.internal>
2013-07-12 08:21:57 +0000

    final version grader.js

--- grader.js
+++ grader.js
@@ -22,8 +22,11 @@ References:
 */
 
 var fs = require('fs');
+var sys = require('util'),
+    rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -64,11 +67,27 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html')
+	.option('--url <html_path>', 'Url to index.html')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.file) {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    } else {
+	rest.get(program.url).on('complete', function(result) {
+        	if (result instanceof Error) {
+                	sys.puts('Error: ' + result.message);
+                	this.retry(5000); // try again after 5 sec
+          	} else {
+			var f = fs.openSync('tmp.' + HTMLFILE_DEFAULT,'w');
+			fs.writeSync(f, new Buffer(result));
+		        var checkJson = checkHtmlFile('tmp.' + HTMLFILE_DEFAULT, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+          	}
+	});
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
